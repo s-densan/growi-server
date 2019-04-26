75d425aa09a23b27d5c677126831fcffd7523e3a
smpi-un <decide_in_the_eyes@growi.us-west1-b.c.s-densan-wiki.internal>
2019-04-26 20:28:41 +0900

    最初のコミット

---
+++ backup.sh
@@ -0,0 +1,14 @@
+backup_root_dir=~/backup
+backup_dir_name=growi_backup_$(date '+%Y%m%d_%H%M%S')
+backup_dir_path=${backup_root_dir}/${backup_dir_name}
+archive_file_name=${backup_dir_name}.tar.xz
+archive_file_path=${backup_root_dir}/${archive_file_name}
+
+mkdir ${backup_dir_path}
+sudo cp -r /var/lib/docker/volumes/growi_growi_data/_data/uploads "${backup_dir_path}"
+mongodump --port 27017 -d growi -u growiuser -p grogro --out "${backup_dir_path}/mongodump_growi"
+tar -Jcvf "${archive_file_path}" -C "${backup_root_dir}" "${backup_dir_name}"
+
+~/go/bin/skicka mkdir /growi
+~/go/bin/skicka mkdir /growi/backup
+~/go/bin/skicka upload "${archive_file_path}" /growi/backup/
