945ee58510bfa4a59e55611dce551576c71ae884
Yakoi <yakoi.rice@gmail.com>
2019-05-01 17:51:45 +0900

    pull

--- pull_from_github.sh
+++ pull_from_github.sh
@@ -1,6 +1,7 @@
 cd ~/source/growi-server
 sudo git fetch
 sudo git reset --hard
+sudo git merge
 cp pull_from_github.sh ~/
 cp backup.sh ~/
 sudo cp crontab /var/spool/cron/crontabs/decide_in_the_eyes
