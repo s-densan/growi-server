8a65df33acd3e9d3ed1cc13b03e0b670ca0a1b25
Yakoi <yakoi.rice@gmail.com>
2019-07-24 21:25:21 +0900

    change upload dir

--- backup.sh
+++ backup.sh
@@ -6,7 +6,8 @@ archive_file_name=${backup_dir_name}.tar.xz
 archive_file_path=${backup_root_dir}/${archive_file_name}
 upload_dir1=/growi
 upload_dir2=${upload_dir1}/backup
-upload_dir3=${upload_dir2}/$(date '+%Y%m')
+upload_dir3=${upload_dir2}/$(date '+%Y')
+upload_dir4=${upload_dir3}/$(date '+%Y%m')
 
 mkdir ${backup_dir_path}
 sudo cp -r /var/lib/docker/volumes/growi_growi_data/_data/uploads "${backup_dir_path}"
@@ -16,7 +17,8 @@ tar -Jcvf "${archive_file_path}" -C "${backup_root_dir}" "${backup_dir_name}"
 ~/go/bin/skicka mkdir ${upload_dir1}
 ~/go/bin/skicka mkdir ${upload_dir2}
 ~/go/bin/skicka mkdir ${upload_dir3}
-~/go/bin/skicka upload "${archive_file_path}" ${upload_dir3}
+~/go/bin/skicka mkdir ${upload_dir4}
+~/go/bin/skicka upload "${archive_file_path}" ${upload_dir4}
 
 # Slack通知
 completion_message="バックアップを取得しました。${archive_file_name}"
